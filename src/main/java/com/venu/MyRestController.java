973628ec84d52602b78166d70c231069cf2aa51a
venugopal durshetty <vdurshetty@gmail.com>
2017-07-22 18:01:48 +1000

    gradle file update

---
+++ src/main/java/com/venu/MyRestController.java
@@ -0,0 +1,38 @@
+package com.venu;
+
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
+
+
+@RestController  
+public class MyRestController {  
+   
+ @RequestMapping(value = "/hello", method = RequestMethod.GET)  
+ public String sayHello()  
+ {  
+  return "Hello! This is a simple Rest Service Example.......";  
+ }  
+  
+
+  
+ @RequestMapping(value = "/SMSResponse", method = RequestMethod.POST,headers="Accept=application/json")  
+ public String logMessage(@RequestBody Object orderResponse)  
+ {  
+	 String status = "Success";
+	   if (orderResponse!=null){
+		   System.out.println("Customer responded with order confirmation!");
+		   System.out.println("The JSON Object " + orderResponse);
+	   } else {
+		   System.out.println("Unable to deliver the Message!");
+		   status = "fail";
+	   }
+    return status;  
+  }
+ 
+ 
+
+ 
+
+}  
